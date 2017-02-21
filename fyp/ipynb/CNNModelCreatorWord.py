f725f18dc86e698f0bba07c0bdebedbba75f43ba
Ninad998 <ninad.tungare@gmail.com>
2017-02-21 16:35:52 +0800

    Latest commit, samples test on old db started

--- fyp/ipynb/CNNModelCreatorWord.py
+++ fyp/ipynb/CNNModelCreatorWord.py
@@ -64,8 +64,11 @@ def loadAuthData(authorList, doc_id, chunk_size = 1000, samples = 300):
     
     for auth in authorList:
         current = textToUse.loc[textToUse['author_id'] == auth]
-        current = current.sample(n = min(size))
-        samples = min(size)
+        if(samples > min(size)):
+            current = current.sample(n = min(size))
+            samples = min(size)
+        else:
+            current = current.sample(n = samples)
         textlist = current.doc_content.tolist()
         texts = texts + textlist
         labels = labels + [authorList.index(author_id) for author_id in current.author_id.tolist()]

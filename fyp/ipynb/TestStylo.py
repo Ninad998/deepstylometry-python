287601a9cd19497467404d7e03aa6caa0bf76ebe
Ninad998 <ninad.tungare@gmail.com>
2017-02-28 00:49:43 +0800

    Updated for condor

--- fyp/ipynb/TestStylo.py
+++ fyp/ipynb/TestStylo.py
@@ -1,44 +1,73 @@
 
-authorList = [11, 18, 80, 88, 64]
+authorList = [11, 18, 80, 88, 64, 44, 91, 19, 97]
+
 doc_id = 1
 
-candidate = len(authorList)
+parameters = {
+    'candidate': [2, 3, 4, 5, 6, 7, 8, 9],
+    'samples': [320, 1600, 3200],
+    'dropout': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5],
+    'dimensions': [50, 100, 200],
+    'iterations': [10, 20, 40, 80, 120, 240],
+    'cv': [320, 1600]#, 3200],
+}
 
-test = "batch10" # change before run
+level = 'word'
 
-level = "word"
+output = []
 
-iterations = 30
+for idxp, paralist in parameters.iteritems():
+    if idxp == 'candidate':
+        for idxl, val in enumerate(paralist):
+            
+            authorList = [11, 18, 80, 88, 64, 44, 91, 19, 97]
+            
+            doc_id = 1
 
-dropout = 0.5
+            candidate = val
 
-samples = 3200
+            test = idxp # change before run
 
-dimensions = 200
+            level = "word"
 
-printstate = (("doc_id = %s, candidate = %s, ") % (str(doc_id), str(candidate)))
-printstate += (("dimensions = %s, samples = %s, ") % (str(dimensions), str(samples)))
-printstate += (("\niterations = %s, dropout = %s, test = %s") % (str(iterations), str(dropout), str(test)))
+            iterations = 30
 
-print("Current test: %s" % (str(printstate)))
+            dropout = 0.5
 
-print("Running: %12s" % (str(printstate)))
+            samples = 3200
 
-import StyloNeural as Stylo
-(labels_index, history, train_acc, val_acc, samples) = Stylo.getResults(
-    doc_id = doc_id, authorList = authorList[:], 
-    level = level, glove = '../../glove/', dimensions = dimensions, 
-    samples = samples, nb_epoch = iterations, dropout = dropout, batch_size = 10 )
+            dimensions = 200
 
-del Stylo
+            # loc = authorList.index(author_id)
 
-output = [
-    [doc_id, candidates, dimensions, samples, 
-     iterations, dropout, train_acc, val_acc, 
-     test], 
-    [doc_id, candidates, dimensions, samples, 
-     iterations, dropout, train_acc, val_acc, 
-     test]]
+            printstate = (("doc_id = %s, candidate = %s, ") % (str(doc_id), str(candidate)))
+            printstate += (("dimensions = %s, samples = %s, ") % (str(dimensions), str(samples)))
+            printstate += (("\niterations = %s, dropout = %s, test = %s") % (str(iterations), str(dropout), str(test)))
+
+            print("Current test: %s" % (str(printstate)))
+            
+            print("Running: %12s" % (str(printstate)))
+            
+            import StyloNeural as Stylo
+            (labels_index, train_acc_list, val_acc_list, samples) = Stylo.getResults(
+                doc_id = doc_id, authorList = authorList[:candidate + 1], 
+                level = level, glove = '../../glove/', dimensions = dimensions, 
+                samples = samples, nb_epoch = iterations, dropout = dropout, batch_size = 10 )
+            
+            output.append([doc_id, candidate, dimensions, samples, 
+                           iterations, dropout, train_acc, val_acc, 
+                           test])
+            
+            del Stylo
+
+            from keras import backend as K
+            K.clear_session()
+
+            import time
+            time.sleep(10)
+
+            from IPython.display import clear_output
+            clear_output()
 
 import pandas as pd
 df = pd.DataFrame(output, columns=["doc_id", "candidates", "dimensions", "samples",

0f7dfbecd8373558fe2a079d23c8619b3795bb5d
Ninad998 <ninad.tungare@gmail.com>
2017-03-02 08:49:57 +0800

    ML files uploaded

--- fyp/ipynb/UpdateDB.py
+++ fyp/ipynb/UpdateDB.py
@@ -165,3 +165,79 @@ def updateresultOldCNNCV(doc_id = 0, candidate = 4, dimensions = 200,
     finally:
         if conn is not None:
             conn.close()
+
+
+def checkOldML(doc_id = 0, candidate = 4, samples = 300,
+               test = 'Error'):
+
+    conn = None
+
+    try:
+        conn = MySQLdb.connect(host="127.0.0.1", user="ninadt", passwd="ninadt", db="tests")
+
+        cursor = conn.cursor()
+
+        query = "SELECT * FROM readingsOldML WHERE doc_id = " + str(doc_id) + " AND candidates = " + str(candidate)
+        query += " AND samples = " + str(samples)
+        query += " AND test LIKE '%" + str(test) + "%' ;"
+
+        cursor.execute(query)
+
+        print("Execution completed")
+        rows = cursor.fetchall()
+
+        if (len(rows) > 0):
+            return True
+        else:
+            return False
+
+    except MySQLdb.Error as e:
+        if conn:
+            conn.rollback()
+        print('Error %s' % e)
+        sys.exit(1)
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+def updateresultOldCNN(doc_id = 0, candidate = 4, samples = 300, 
+                       train_acc = 0.0, val_acc = 0.0, test = 'Error'):
+
+    conn = None
+
+    try:
+        conn = MySQLdb.connect(host="127.0.0.1", user="ninadt", passwd="ninadt", db="tests")
+
+        cursor = conn.cursor()
+        
+        query = "SELECT * FROM readingsOldML WHERE doc_id = " + str(doc_id) + " AND candidates = " + str(candidate)
+        query += " AND samples = " + str(samples)
+        query += " AND test LIKE '%" + str(test) + "%' ;"
+
+        cursor.execute(query)
+
+        print("Execution completed")
+        rows = cursor.fetchall()
+
+        if (len(rows) > 0):
+            return False
+        else:
+            cursor.execute("""INSERT INTO readingsOldML
+            (doc_id, candidates, samples, train_acc, val_acc, test)
+            VALUES (%s, %s, %s, %s, %s, %s); """,
+                           (str(doc_id), str(candidate), str(samples), 
+                            str(train_acc), str(val_acc), str(test)))
+            conn.commit()
+
+            return True
+
+    except MySQLdb.Error as e:
+        if conn:
+            conn.rollback()
+        print('Error %s' % e)
+        sys.exit(1)
+
+    finally:
+        if conn is not None:
+            conn.close()
